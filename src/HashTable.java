078655699b5facc3b9d42bdfed2c05c74ededd20
Nate Allen <nkallen@iastate.edu>
2018-03-03 13:35:14 -0600

    Skeleton code

---
+++ src/HashTable.java
@@ -0,0 +1,76 @@
+// LEAVE THIS FILE IN THE DEFAULT PACKAGE
+//  (i.e., DO NOT add 'package cs311.pa1;' or similar)
+
+// DO NOT MODIFY THE EXISTING METHOD SIGNATURES
+//  (you may, however, add member fields and additional methods)
+
+// DO NOT INCLUDE LIBRARIES OUTSIDE OF THE JAVA STANDARD LIBRARY
+//  (i.e., you may include java.util.ArrayList etc. here, but not junit, apache commons, google guava, etc.)
+
+import java.util.ArrayList;
+
+/**
+* @author Hugh Potter
+*/
+
+public class HashTable
+{
+	// member fields and other member methods
+
+	public HashTable(int size)
+	{
+		// implementation
+	}
+
+	public int maxLoad()
+	{
+		// implementation
+		return 0;
+	}
+
+	public float averageLoad()
+	{
+		// implementation
+		return 0;
+	}
+
+	public int size()
+	{
+		// implementation
+		return 0;
+	}
+
+	public int numElements()
+	{
+		// implementation
+		return 0;
+	}
+
+	public float loadFactor()
+	{
+		// implementation
+		return 0;
+	}
+
+	public void add(Tuple t)
+	{
+		// implementation
+	}
+
+	public ArrayList<Tuple> search(int k)
+	{
+		// implementation
+		return null;
+	}
+
+	public int search(Tuple t)
+	{
+		// implementation
+		return 0;
+	}
+
+	public void remove(Tuple t)
+	{
+		// implementation		
+	}
+}
