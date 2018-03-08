d948003e87b27b086675bd076b65897ee595ff08
pchihak <pchihak@gmail.com>
2018-03-08 15:50:46 -0600

    HashStringSimilarity works!

--- src/HashStringSimilarity.java
+++ src/HashStringSimilarity.java
@@ -16,19 +16,14 @@ import java.util.ArrayList;
 public class HashStringSimilarity
 {
 	// member fields and other member methods
-	String s1, s2;
-	int sLength;
-	float s1Len, s2Len;
-	final int ALPHA = 31;
-	private ArrayList<String> S = new ArrayList<String>();
-	private ArrayList<String> T = new ArrayList<String>();
-	private ArrayList<String> U = new ArrayList<String>();
-	HashTable tableS1 = new HashTable(50);
-	HashTable tableS2 = new HashTable(50);
+	private ArrayList<String> S = new ArrayList<>();
+	private ArrayList<String> T = new ArrayList<>();
+	private ArrayList<String> U = new ArrayList<>();
+	private HashTable tableS1 = new HashTable(50);
+	private HashTable tableS2 = new HashTable(50);
 
 	public HashStringSimilarity(String s1, String s2, int sLength)
 	{
-		this.sLength = sLength;
 		for (int i = 0; i < (s1.length() - sLength) + 1; i++){
 			String toAdd = s1.substring(i, i + sLength);
 			S.add(toAdd);
@@ -46,35 +41,26 @@ public class HashStringSimilarity
 		createTables();
 	}
 
-	private void createTables(){
-		int key;
-		for(String shingle : S){
-			key = 0;
-			for(int i = 0; i < shingle.length(); i++){
-				key += shingle.charAt(i);
-			}
-			Tuple tuple = new Tuple(key, shingle);
-			tableS1.add(tuple);
-		}
-		for(String shingle : T){
-			key = 0;
-			key = strValue(shingle);
-			Tuple tuple = new Tuple(key, shingle);
-			tableS2.add(tuple);
-		}
+	public float lengthOfS1()
+	{
+		return vectorLength(tableS1);
 	}
 
-	private int duplicate(Tuple shingle, ArrayList<Tuple> tuples) {
-		int count = 0;
-		for (Tuple value : tuples) {
-			if (value.equals(shingle)) {
-				count++;
-			}
-		}
-		return count;
+	public float lengthOfS2()
+	{
+		return vectorLength(tableS2);
 	}
 
-	public float vectorLength(HashTable table){
+	public float similarity()
+	{
+		float similarity = 0;
+		for(String substring : U){
+			similarity += similarityHelper(substring);
+		}
+		return similarity / (vectorLength(tableS1) * vectorLength(tableS2));
+	}
+
+	private float vectorLength(HashTable table){
 		float vectorLength = 0;
 		for(ArrayList<Tuple> bucket : table.buckets){
 			if(bucket != null){
@@ -91,6 +77,33 @@ public class HashStringSimilarity
 		return (float) Math.sqrt(vectorLength);
 	}
 
+	private void createTables(){
+		for(String shingle : S){
+			tableS1.add(new Tuple(strValue(shingle), shingle));
+		}
+		for(String shingle : T){
+			tableS2.add(new Tuple(strValue(shingle), shingle));
+		}
+	}
+
+	private int duplicate(Tuple shingle, ArrayList<Tuple> tuples) {
+		int count = 0;
+		for (Tuple value : tuples) {
+			if (value.equals(shingle)) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	private int similarityHelper(String str){
+		int count = 0;
+		Tuple tuple = new Tuple(strValue(str), str);
+		count += tableS1.search(tuple);
+		count *= tableS2.search(tuple);
+		return count;
+	}
+
 	private int strValue(String str){
 		int key = 0;
 		for(int i = 0; i < str.length(); i++){
@@ -99,38 +112,10 @@ public class HashStringSimilarity
 		return key;
 	}
 
-	public float lengthOfS1()
-	{
-		return vectorLength(tableS1);
-	}
-
-	public float lengthOfS2()
-	{
-		return vectorLength(tableS2);
-	}
-
-	private int helper(String str){
-		int count = 0;
-		Tuple tuple = new Tuple(strValue(str), str);
-		count += tableS1.search(tuple);
-		tuple = new Tuple(strValue(str), str);
-		count *= tableS2.search(tuple);
-		return count;
-	}
-
-	public float similarity()
-	{
-		float similarity = 0;
-		for(String substring : U){
-			similarity += helper(substring);
-		}
-		return similarity / (vectorLength(tableS1) * vectorLength(tableS2));
-	}
-
     public static void main(String[] args){
        HashStringSimilarity test = new HashStringSimilarity("aroseisaroseisarose", "aroseisaflowerwhichisarose", 4);
-       test.tableS1.printTable(false);
-       System.out.println(test.lengthOfS1());
+       System.out.println(Math.pow(test.lengthOfS1(), 2));
+       System.out.println(Math.pow(test.lengthOfS2(), 2));
        System.out.println(test.similarity());
     }
 }
