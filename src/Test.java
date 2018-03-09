0a2ae8103ea5aa6918dfab5c51f05ee56e067fbf
pchihak <pchihak@gmail.com>
2018-03-08 21:31:34 -0600

    my latest version

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,9 @@
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Test {
     String s1, s2;
-    public Test(String file1, String file2, int shingleLength)
+    public Test(String file1, String file2)
     {
         try {
             Scanner scanner = new Scanner(new File(file1));
@@ -26,14 +21,8 @@ public class Test {
         }
     }
 
-    static String readFile(String path, Charset encoding) throws IOException
-    {
-        byte[] encoded = Files.readAllBytes(Paths.get(path));
-        return new String(encoded, encoding);
-    }
-
     public static void main(String[] args){
-        Test test = new Test("test/shak1", "test/shak2", 8);
+        Test test = new Test("test/shak1", "test/shak2");
 
         long time = System.currentTimeMillis();
         HashStringSimilarity string = new HashStringSimilarity(test.s1, test.s2, 7);
